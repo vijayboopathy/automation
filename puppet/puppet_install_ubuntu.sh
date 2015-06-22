46af1963312a2e420b1f971048c0ade357f3f99f
Gourav Shah <gs@initcron.org>
2015-06-22 17:25:28 +0530

    updated script to install puppet agent on ubuntu

--- puppet/puppet_install_ubuntu.sh
+++ puppet/puppet_install_ubuntu.sh
@@ -1,10 +1,50 @@
 #!/bin/bash
+PUPPET_MASTER= 
+CERTNAME= 
+
+SCRIPT=`basename ${BASH_SOURCE[0]}`
+PUPPET_MASTER= 
+CERTNAME= 
+function USAGE {
+  echo -e "\\n-------------------------------------------------------------"\\n
+  echo -e "Usage:$SCRIPT -m master_hostname -c certname_to_register"\\n
+  echo "Command line switches are must. The following switches are recognized."
+  echo "-m  --puppet master fqdn/hostname "
+  echo "-c  --name to register this node with puppet master as e.g. node1.example.org "
+  echo -e "-h  --Displays this help message. No further functions are performed."\\n
+  echo -e "Example: $SCRIPT -m puppet.example.org  -c node1.example.org "\\n
+  echo -e "\\n-------------------------------------------------------------"\\n
+  exit 1
+}
+
+NUMARGS=$#
+if [ $NUMARGS -lt 2 ]; then
+  USAGE
+fi
+
+while getopts mc: OPTION; do
+  case $OPTION in
+    m)
+      PUPPET_MASTER=$OPTARG
+      ;;
+    c)  
+      CERTNAME=$OPTARG
+      ;;
+    h) 
+      USAGE
+      ;;
+  esac
+done
+
+echo "I: Setting up APT Repository for Puppet..."
 wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
 sudo dpkg -i puppetlabs-release-precise.deb
 sudo apt-get update
+echo "I: Installing Puppet..."
 sudo apt-get install puppet -yq
-sudo sed -i 's/no/yes/g' /etc/default/puppet
-cat << EOF >> /etc/puppet/puppet.conf
+
+echo"I: Generate puppet configurations..."
+cat << EOF > /etc/puppet/puppet.conf
 [agent]
 
     report        = true
@@ -12,6 +52,7 @@ cat << EOF >> /etc/puppet/puppet.conf
     masterport    = 8140
     environment   = production
     server        = $PUPPET_MASTER
+    certname      = $CERTNAME
     listen        = false
     splay         = false
     runinterval   = 1800
@@ -19,6 +60,13 @@ cat << EOF >> /etc/puppet/puppet.conf
     configtimeout = 120
 EOF
 
-/etc/init.d/puppet restart
-sudo puppet agent -t
+echo "I: Enable Puppet as a Service..."
+sudo sed -i 's/no/yes/g' /etc/default/puppet
+
+echo "I: Starting Puppet Agent after enabling...."
+/etc/init.d/puppet start
+
+echo "I: Launching first run of Puppet Agent...."
+sudo puppet agent -t --waitforcert 10
+
 
