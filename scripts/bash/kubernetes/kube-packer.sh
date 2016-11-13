b8b101eefb05e2c29a14dc12167a73f3496ed280
Venkat-Sudharsanam <vishal.ronaldo93@gmail.com>
2016-11-13 12:47:56 +0530

    For Packer

---
+++ scripts/bash/kubernetes/kube-packer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update -y
+apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni
+touch /master.sh
+cat <<EOF > /master.sh
+touch adm
+touch minion.sh
+echo '#!/bin/bash' >> minion.sh
+kubeadm init >> adm
+tail adm -n 1 >> minion.sh
+echo "Master configuration is done!"
+sleep 3
+clear
+echo "Run the following command on all the node which you need to configure it as an minion"
+cat minion.sh
+sleep 5
+EOF
